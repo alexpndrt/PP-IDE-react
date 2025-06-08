85f563f7a0fd22fa0c4d62bd91914d0102b92c69
Alex PONDART <alex.pondart@oclock.school>
2025-06-08 06:41:13 +0200

    feat => create components static Tabs.jsx

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,14 @@
+export default function Tabs() {
+  return (
+    <div className="flex grow">
+      <div className="grow flex flex-col w-[175px] shrink-0 text-slate-300 border-r border-slate-200">
+        <button>HTML</button>
+        <button>CSS</button>
+        <button>JS</button>
+      </div>
+      <div className="w-full grow relative">
+        <p>Le code ...</p>
+      </div>
+    </div>
+  );
+}
